a6d405c745c8a897f57e2ff022b278a3267966a8
Aaron negrete <aaronnek1@gmail.com>
2017-11-28 16:25:42 +0000

    quitar errores y warnings #4

--- editar.php
+++ editar.php
@@ -1,7 +1,7 @@
 <?php session_start(); 
 //datos para establecer la conexion con la base de mysql.
-require "../cfg/conexion.php";
-
+require "./cfg/conexion.php";
+error_reporting(0);
 // verificamos si se han enviado ya las variables necesarias.
 if (isset($_GET["nom"])) {
     $name = $_GET["nom"];
@@ -24,7 +24,7 @@ if (isset($_GET["nom"])) {
                 $query = 'UPDATE SALA_REMOTA SET nombre="'.$name.'", responsable="'.$responsable.'", telefono="'.$telefono.'", email_responsable="'.$email.'", ip="'.$ip.'", servicios="'.$servicios.'" WHERE id_sala_remota='.$id;
                 mysql_query($query) or die(mysql_error());
                 //echo 'La sala '.$name.' se ha modificado satisfactoria.<br/>';
-   header('Location: listar.php');
+   header('Location: index.php');
             }
         }
 
--- editarSala.php
+++ editarSala.php
@@ -92,15 +92,15 @@ if (isset($_GET["id"])) {
 </div>
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="js/jquery-1.11.0.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
-    <!-- Metis Menu Plugin JavaScript -->
+    <!-- Metis Menu Plugin JavaScript >
     <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
 
-    <!-- Custom Theme JavaScript -->
-    <script src="js/sb-admin-2.js"></script>
+    <!-- Custom Theme JavaScript >
+    <script src="js/sb-admin-2.js"></script-->
 
 </body>
