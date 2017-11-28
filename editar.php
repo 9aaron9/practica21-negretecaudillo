e94412926662ece937bfdba86aa58f4c19ad4a88
Aaron negrete <aaronnek1@gmail.com>
2017-11-28 16:11:01 +0000

    agregar campo servicios #6

--- editar.php
+++ editar.php
@@ -9,18 +9,19 @@ if (isset($_GET["nom"])) {
     $telefono = $_GET["tel"];
     $email = $_GET["mail"];
     $ip = $_GET["ip"];
+    $servicios = $_GET["servicios"];
     $id = $_GET["id"];
     
     
     
     
     // Hay campos en blanco
-    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL) {
+    if($name==NULL|$responsable==NULL|$telefono==NULL|$email==NULL|$ip==NULL|$servicios==NULL) {
         echo "un campo est&aacute; vacio.";
         formRegistro();
     }else{
 
-                $query = 'UPDATE SALA_REMOTA SET nombre="'.$name.'", responsable="'.$responsable.'", telefono="'.$telefono.'", email_responsable="'.$email.'", ip="'.$ip.'" WHERE id_sala_remota='.$id;
+                $query = 'UPDATE SALA_REMOTA SET nombre="'.$name.'", responsable="'.$responsable.'", telefono="'.$telefono.'", email_responsable="'.$email.'", ip="'.$ip.'", servicios="'.$servicios.'" WHERE id_sala_remota='.$id;
                 mysql_query($query) or die(mysql_error());
                 //echo 'La sala '.$name.' se ha modificado satisfactoria.<br/>';
    header('Location: listar.php');
